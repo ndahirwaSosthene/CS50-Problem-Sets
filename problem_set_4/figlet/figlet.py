cc01b4fc16cf4048530de60f0d696b74e0d44315
Ndahirwa Sosthene <ndahirwasosthene@gmail.com>
2025-07-08 08:08:12 +0200

    Add my own flag to list all fonts

--- problem_set_4/figlet/figlet.py
+++ problem_set_4/figlet/figlet.py
@@ -6,13 +6,20 @@ import random
 figlet = Figlet()
 parser = argparse.ArgumentParser(description="Create your custom figlets:  Frank, Ian, and Glen’s letters")
 parser.add_argument("-f","--font", help="Set font for your figlet text")
+parser.add_argument("-l","--list", help="view all fonts available", action="store_true")
 args , unknown= parser.parse_known_args()
+
+if args.list:
+    for font in figlet.getFonts():
+        print(font)
+    sys.exit(0)
 
 if args.font:
     figlet.setFont(font = args.font)
 elif unknown:
     print("Invalid usage")
     sys.exit(1)
+
 else:
     figlet.setFont(font=random.choice(figlet.getFonts()))
 
