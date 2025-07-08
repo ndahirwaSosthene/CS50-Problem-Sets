365636c0bb756ce90d7d6b7ec32be30de91fd983
Ndahirwa Sosthene <ndahirwasosthene@gmail.com>
2025-07-08 08:00:07 +0200

    Add check for unknown arguments

--- problem_set_4/figlet/figlet.py
+++ problem_set_4/figlet/figlet.py
@@ -6,10 +6,13 @@ import random
 figlet = Figlet()
 parser = argparse.ArgumentParser(description="Create your custom figlets:  Frank, Ian, and Glen’s letters")
 parser.add_argument("-f","--font", help="Set font for your figlet text")
-args = parser.parse_args()
+args , unknown= parser.parse_known_args()
 
 if args.font:
     figlet.setFont(font = args.font)
+elif unknown:
+    print("Invalid usage")
+    sys.exit(1)
 else:
     figlet.setFont(font=random.choice(figlet.getFonts()))
 
