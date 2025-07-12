b5fd6277d68005d3be25ab79ce5c673ca9b8181f
Ndahirwa Sosthene <ndahirwasosthene@gmail.com>
2025-07-12 11:10:46 +0200

    Fix the API request

--- problem_set_4/bitcoin/bitcoin.py
+++ problem_set_4/bitcoin/bitcoin.py
@@ -1,7 +1,9 @@
 import sys
 import requests
 
-API_KEY = "2117cd83ca1434751aaff34bbafecf1a7e9118e3b44f68faa7d4b48ae5c8e6ad"
+
+with open("API.txt" ,"r") as file:
+    API_KEY = file.read()
 
 if len(sys.argv) < 2:
     print("Missing command-line argument")
