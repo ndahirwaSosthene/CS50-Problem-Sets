32092ccf288f94272813097d2e3f5a42d4b0689a
Ndahirwa Sosthene <ndahirwasosthene@gmail.com>
2025-07-15 20:12:53 +0200

    Add twttr tests

--- problem_set_5/twttr/twttr.py
+++ problem_set_5/twttr/twttr.py
@@ -4,13 +4,15 @@ def main():
     print(short)
 
 def shorten(word):
-    for c in word:
-        if c in ['a','e','i','u','o','A','E','I','U','O']:
-            word = word.replace(c, "")
-        else:
-            exit
-    return word
-
+    if word.isalpha():
+        for c in word:
+            if c in ['a','e','i','u','o','A','E','I','U','O']:
+                word = word.replace(c, "")
+            else:
+                exit
+        return word
+    else:
+        exit(1)
 if __name__ == "__main__":
     main()
 
